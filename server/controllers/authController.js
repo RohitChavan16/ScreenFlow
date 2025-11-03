ebf5f8da940376639f9532ccd41bf37bda2e288e
Rohit Chavan <approachrohit16@gmail.com>
2025-11-03 09:57:55 +0530

    fixing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,6 +26,7 @@ res.cookie('token', token, {
 httpOnly: true,
 secure: process.env.NODE_ENV === 'production',
 sameSite: process.env.NODE_ENV == 'production' ? 'none': 'strict',
+domain: ".vercel.app",
 maxAge: 7 * 24 * 60 * 60 * 1000 });
  
 // Send the email to new user register
@@ -80,6 +81,7 @@ res.cookie('token', token, {
 httpOnly: true,
 secure: process.env.NODE_ENV === 'production',
 sameSite: process.env.NODE_ENV == 'production' ? 'none': 'strict',
+domain: ".vercel.app",
 maxAge: 7 * 24 * 60 * 60 * 1000 });
 
 return res.json({success: true});
